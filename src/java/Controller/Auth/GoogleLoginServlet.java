67280c48f06b7a54ed45a26270c42ee14cb22456
Kien01102004 <Kien01.10.2004@gmail.com>
2026-02-01 22:31:09 +0700

    update commit before - Kien

---
+++ src/java/Controller/Auth/GoogleLoginServlet.java
@@ -0,0 +1,38 @@
+package Controller.Auth;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.ServletException;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class GoogleLoginServlet extends HttpServlet {
+
+    // TODO: thay bang CLIENT_ID cua ban
+    private static final String CLIENT_ID = "546134683118-2hnlovqa88eg8ol483e9gp5lsm2av7pn.apps.googleusercontent.com";
+    private static final String REDIRECT_URI = "http://localhost:8080/FifaField/oauth2/callback";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String state = java.util.UUID.randomUUID().toString();
+        req.getSession(true).setAttribute("oauth_state", state);
+
+        String scope = "openid email profile";
+
+        String url = "https://accounts.google.com/o/oauth2/v2/auth"
+                + "?client_id=" + enc(CLIENT_ID)
+                + "&redirect_uri=" + enc(REDIRECT_URI)
+                + "&response_type=code"
+                + "&scope=" + enc(scope)
+                + "&state=" + enc(state)
+                + "&access_type=online"
+                + "&prompt=select_account";
+
+        resp.sendRedirect(url);
+    }
+
+    private String enc(String s) {
+        return URLEncoder.encode(s, StandardCharsets.UTF_8);
+    }
+}
